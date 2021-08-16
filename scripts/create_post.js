d13d8407d061fa693124589b052048cfbd14052a
Brian3647 <victoris3647@gmail.com>
2021-08-16 13:37:32 +0200

    Add script

---
+++ scripts/create_post.js
@@ -0,0 +1,32 @@
+/**
+*	@type {Array<string>}
+*/
+const argv = process.argv.slice(2);
+
+/**
+ * @type {Date}
+ */
+const now = new Date();
+
+const config = {
+	title: argv[0],
+	description: argv[1],
+	tags: argv[2],
+	categories: argv[3],
+	date: `${now.getFullYear()}-${
+		now.getMonth().toString().length > 1 ? now.getMonth() + 1 : '0' + (now.getMonth() + 1)
+	}-${now.getDate()}`
+};
+
+const res =`
++++
+title = "${config.title}"
+date = ${config.date}
+
+[taxonomies]
+tags = ${config.tags}
+categories = ${config.categories}
++++
+`.trim();
+
+require('fs').writeFileSync('./content/' + config.title + '.md', res);
